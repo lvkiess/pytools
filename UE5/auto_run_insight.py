7b1008d2ded08a5c4a5493afd34f86666c070088
ccy <klzycy@live.cn>
2024-07-18 16:07:03 +0800

    根据现场实际，证实可行性

--- UE5/auto_run_insight.py
+++ UE5/auto_run_insight.py
@@ -26,7 +26,16 @@ def open_local_program(path):
 
 def start_app(package_name):
     try:
-        subprocess.check_call(["adb", "shell", "am", "start", "-n", package_name + "/com.epicgames.unreal.SplashActivity"])
+        subprocess.check_call(
+            [
+                "adb",
+                "shell",
+                "am",
+                "start",
+                "-n",
+                package_name + "/com.epicgames.unreal.SplashActivity",
+            ]
+        )
         print(f"Started app: {package_name}")
     except subprocess.CalledProcessError as e:
         print(f"Failed to start app: {e}")
@@ -47,10 +56,14 @@ def check_utrace_file(path):
                 file_path = os.path.join(path, file)
 
                 # 获取文件的修改时间
-                modified_timestamp = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+                modified_timestamp = datetime.datetime.fromtimestamp(
+                    os.path.getmtime(file_path)
+                )
 
                 # 如果文件的修改时间晚于当前时间戳减去5秒，则认为文件是新生成的
-                if modified_timestamp >= current_timestamp - datetime.timedelta(seconds=5):
+                if modified_timestamp >= current_timestamp - datetime.timedelta(
+                    seconds=10
+                ):
                     return True
 
         return False
@@ -61,19 +74,22 @@ def check_utrace_file(path):
 if __name__ == "__main__":
     if is_android_device_connected():
         # 执行两条指令
-        execute_adb_command("adb reverse tcp:1980 tcp:1980")
-        execute_adb_command("adb shell setprop debug.ue.commandline -tracehost=127.0.0.1")
+        execute_adb_command("adb reverse tcp:1981 tcp:1981")
+        # execute_adb_command(
+        #     "adb shell setprop debug.ue.commandline -tracehost=127.0.0.1"
+        # )
 
         # 打开本地的UnrealInsights程序
-        local_program_path = "H:\\UE_5.4\\Engine\\Binaries\\Win64\\UnrealInsights.exe"
+        local_program_path = "C:\\Program Files\\Epic Games\\UE_5.4\\Engine\\Binaries\\Win64\\UnrealInsights.exe"
         open_local_program(local_program_path)
 
         # 打开Android设备上的名为“TRing”的app
-        app_package_name = "com.Tencent.TRing"
+        app_package_name = "com.Tencent.TRingDev"
         start_app(app_package_name)
 
         # 检查本地路径下是否生成了新的.utrace文件
-        utrace_folder_path = "C:\\Users\\chongyechen\\AppData\\Local\\UnrealEngine\\Common\\UnrealTrace\\Store\\001"
+        utrace_folder_path = "C:\\Users\\admin\\AppData\\Local\\UnrealEngine\\Common\\UnrealTrace\\Store\\001"
+
         if check_utrace_file(utrace_folder_path):
             print("新的.utrace文件已生成。")
         else:
